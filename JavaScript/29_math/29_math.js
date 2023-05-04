a2d75c2f1630854b96374b0d132c7d9f2e291078
baechanghyeon12 <qo99033@naver.com>
2023-05-04 17:42:22 +0900

    0504 js

---
+++ JavaScript/29_math/29_math.js
@@ -0,0 +1,11 @@
+console.log( Math.ceil(3.33) );
+console.log( Math.round(3.5) );
+console.log( Math.floor(3.8) );
+
+
+
+// 0~1 사이의 수를 랜덤으로 가져옴
+console.log( Math.ceil(Math.random(  )*10));
+console.log( Math.floor(Math.random(  )*10)+1);
+
+// random() 메소드를 이용해서 1~10까지 랜덤으로 나오게 해주세요.
